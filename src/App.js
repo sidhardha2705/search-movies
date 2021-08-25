cf3f38793fc5232a1fcb3c964987853f0e7ece0c
Sidhardha Kadamati <36865844+sidhardha2705@users.noreply.github.com>
2021-08-25 14:17:25 +0200

    Github pages and react-router conflict fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import SingleMovie from "./components/templates/single-movie";
 import HomePage from "./components/templates/homepage";
 import ViewedMovies from "./contexts/contexts";
